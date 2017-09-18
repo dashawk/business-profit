1138ee7e6e7d74adfd1c18d23775e49e79c17ab9
Jason P <jmpanugaling@gmail.com>
2017-09-18 15:49:12 +0800

    first commit

---
+++ business-profit.js
@@ -0,0 +1,56 @@
+;(function () {
+	'use strict';
+	
+	angular
+		.module('business-profit', [])
+		.service('profit', function () {
+			var service = this;
+			
+			service.getSellPrice = getSellPrice;
+			service.getGrossProfit = getGrossProfit;
+			service.getInclusive = getInclusive;
+			service.getPriceWithQuantity = getPriceWithQuantity;
+			
+			return service;
+			
+			function getGrossProfit(sellPrice, buyPrice) {
+				return (1 - (_n(buyPrice) / _n(sellPrice))) * 100;
+			}
+			function getSellPrice(buyPrice, grossProfit) {
+				return _n(buyPrice) / ((100 - _n(grossProfit)) / 100);
+			}
+			function getInclusive(price, percentage) {
+				return _n(price) * _rate(percentage);
+			}
+			function getPriceWithQuantity(price, quantity) {
+				return _n(price) * _n(quantity);
+			}
+			
+			/**
+			 * Converts string types to numeric types
+			 * @param data
+			 * @returns {number}
+			 * @private
+			 */
+			function _n(data) {
+				return +data;
+			}
+			
+			/**
+			 * Convert Percentage to Tax Rate
+			 * @param percentage
+			 * @returns {number}
+			 * @private
+			 */
+			function _rate(percentage) {
+				var rate = 1;
+				if (angular.isString(percentage)) {
+					if (/%/g.test(percentage)) {
+						rate = _n(percentage.remove(/%/g, ''));
+					}
+				}
+				
+				return (1 + (rate / 100))
+			}
+		});
+}());
